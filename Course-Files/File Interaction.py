3aecf7273291655243d2237cf3535fb839358cbf
V Yashwanth Raj <99544533+YashwanthRaj@users.noreply.github.com>
2022-05-21 18:25:01 +0530

    Update File Interaction.py

--- Course-Files/File Interaction.py
+++ Course-Files/File Interaction.py
@@ -2,22 +2,18 @@
 
 # Creating a file using Write mode 
 # In write mode , if a file with same name is alerady present , it wil delete the file and create new one 
-'''
 file_name = input('Enter file name: ')
 fileA=open(file_name,'w')  
 fileA.write('Hello Everyone !! , We have successfully cerated the file')
 fileA.close()
 print()
-'''
 
 # File writing from list of content
-'''
 file_name=input('Enter your file name: ')
 names=['Yashwanth is a good boy \n','Rahul is a good boy \n','Musk is a good boy \n']
 fileA=open(file_name,'w')
 fileA.write('The file is successfully created !! \n')
 fileA.writelines(names)
-'''
 
 # File Reading the file
 fileA=open('names.txt','r')
